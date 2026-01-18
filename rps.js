e161303d2be414748d2ee210f74ca1cd70870f16
spinch <134818917+leftovertoast@users.noreply.github.com>
2026-01-18 00:27:29 -0500

    write human input guess section

--- rps.js
+++ rps.js
@@ -11,4 +11,23 @@ function getComputerChoice() {
    }
 }
 
-console.log(getComputerChoice());
+console.log("Welcome to Rock, Paper, Scissors!");
+let computerChoice = getComputerChoice();
+console.log("Computer has made its choice, make yours now!");
+
+function getHumanChoice() {
+    let humanChoice = prompt("(R)ock, (P)aper, (S)cissors? (R,P,S)?");
+    if (humanChoice.toLowerCase() === "r" ) {
+        return "rock";
+    } else if (humanChoice.toLowerCase() === "p") {
+        return "paper";
+    } else if (humanChoice.toLowerCase() === "s") { 
+        return "scissors";
+    } else {
+        return "invalid entry, please try again";
+    }
+}
+
+console.log("You chose " + getHumanChoice());
+
+
